f28e12c770127627e60a6dfaa4a0197c22d7ab10
imele <ivox353@gmail.com>
2025-03-10 11:42:44 +0700

    feat: update header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component, HostBinding } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { VideoStateService } from '../../services/video-state.service';
 
 @Component({
@@ -9,8 +13,11 @@ import { VideoStateService } from '../../services/video-state.service';
   standalone: true,
   imports: [
     RouterLink,
+    NgFor,
+    NgIf,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -18,9 +25,33 @@ import { VideoStateService } from '../../services/video-state.service';
 export class HeaderComponent {
   @HostBinding('class.video-finished') isVideoFinished = false;
 
-  projectName = 'Ivo Mele | Software Engineer'; // You can change this to your actual project name
+  projectName = 'Ivo Mele | Software Engineer';
+  
+  frameworks = [
+    { name: 'Angular', icon: 'angular', url: '' },
+    { name: 'React', icon: 'react', url: '' },
+    { name: 'Flutter', icon: 'flutter', url: '' }
+  ];
 
-  constructor(private videoStateService: VideoStateService) {
+  constructor(
+    private videoStateService: VideoStateService,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
+  ) {
     this.isVideoFinished = this.videoStateService.videoFinished$();
+    
+    
+    this.iconRegistry.addSvgIcon(
+      'angular',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/angular.svg')
+    );
+    this.iconRegistry.addSvgIcon(
+      'react',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/react.svg')
+    );
+    this.iconRegistry.addSvgIcon(
+      'flutter',
+      this.sanitizer.bypassSecurityTrustResourceUrl('assets/icons/flutter.svg')
+    );
   }
 }
